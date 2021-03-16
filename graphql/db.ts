e898f1580c7f5af01c848ac7f1b9dce71237412b
JaeHyung_iMac <jhpark.main@gmail.com>
2021-03-16 17:52:50 +0900

    Making Movie API with GraphQL and node-fetch

--- graphql/db.ts
+++ graphql/db.ts
@@ -1,51 +1,18 @@
-export let movies = [
-  {
-    id: 0,
-    name: "Star Wars - The new one",
-    score: 1
-  },
-  {
-    id: 1,
-    name: "Avengers - The new one",
-    score: 8
-  },
-  {
-    id: 2,
-    name: "The Godfather I",
-    score: 9
-  },
-  {
-    id: 3,
-    name: "Logan",
-    score: 2
+import fetch from "node-fetch";
+
+const API_URL = "https://yts.mx/api/v2/list_movies.json";
+
+export const getMovies = (limit: number, rating: number) => {
+  let REQUEST_URL = API_URL;
+
+  if (limit > 0) {
+    REQUEST_URL += `?limit=${limit}`;
   }
-];
-
-export const getMovies = () => movies;
-
-export const getById = (id: number): {} => {
-  const filteredPeople = movies.filter(movie => movie.id === id);
-  return filteredPeople[0];
-};
-
-export const deleteMovie = (id: number): boolean => {
-  const cleanedMovie = movies.filter(movie => movie.id !== id);
-  if (movies.length > cleanedMovie.length) {
-    movies = cleanedMovie;
-    return true;
-  } else {
-    return false;
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
   }
-};
 
-export const addMovie = (name: string, score: number): {} => {
-  const newMovie = {
-    id: movies.length,
-    name,
-    score
-  };
-
-  movies.push(newMovie);
-
-  return newMovie;
+  return fetch(`${REQUEST_URL}`)
+    .then(res => res.json())
+    .then(json => json.data.movies);
 };
--- graphql/resolvers.ts
+++ graphql/resolvers.ts
@@ -1,13 +1,8 @@
-import { getMovies, getById, addMovie, deleteMovie } from "./db";
+import { getMovies } from "./db";
 
 const resolvers = {
   Query: {
-    movies: () => getMovies(),
-    movie: (_: any, { id }) => getById(id)
-  },
-  Mutation: {
-    addMovie: (_: any, { name, score }) => addMovie(name, score),
-    deleteMovie: (_: any, { id }) => deleteMovie(id)
+    movies: (_: any, { limit, rating }) => getMovies(limit, rating)
   }
 };
 
